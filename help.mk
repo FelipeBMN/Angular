92667707f7c70b199cd4f390a06778b343440709
FelipeBMN <felipebrunomn@gmail.com>
2022-06-09 23:13:51 -0300

    header

---
+++ help.mk
@@ -0,0 +1,48 @@
+# Comandos para iniciar um projeto com angular
+
+## Comando para habilitar criar servidor local
+
+    cd backend
+    npm i json-server
+    npm init -y
+
+Alterar o arquivo package.json para:
+
+    {
+    "dependencies": {
+        "json-server": "^0.17.0"
+    },
+    "name": "backend",
+    "version": "1.0.0",
+    "main": "index.js",
+    "devDependencies": {},
+    "scripts": {
+        "start": "json-server --watch db.json --port 3001"
+    },
+    "keywords": [],
+    "author": "",
+    "license": "ISC",
+    "description": ""
+    }
+
+Para rodar o servidor:
+
+    npm start
+
+## Angular
+
+Instalando angular:
+
+    npm i -g @angular/cli 
+
+Criando projeto:
+
+    ng new frontend
+
+Na pasta frontend:
+
+    npm start
+
+Gerando componente:
+
+    ng g c components/template/header
